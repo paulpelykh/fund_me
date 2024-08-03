70ba3e2e6c6b34a2c9482c1838539ba451b3715c
paulpelykh <pelykhpaul@gmail.com>
2024-08-03 12:00:33 +0300

    feat: add mappping to store how much eth send

--- FundMe.sol
+++ FundMe.sol
@@ -8,8 +8,13 @@ contract FundMe {
 
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256) public addressToAmountFunded;
+
     function fund() public payable {
         require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     // function withdraw() public {}
