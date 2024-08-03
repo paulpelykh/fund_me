82c2f8556676c64f8ed46095f0a930b4d48bb4a8
paulpelykh <pelykhpaul@gmail.com>
2024-08-03 17:26:40 +0300

    feat: add withdraw functionality

--- FundMe.sol
+++ FundMe.sol
@@ -14,8 +14,27 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw() public {}
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
 }
